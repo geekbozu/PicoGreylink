b14cef432f3c3d4fa815103572489d5a997f279d
Tim Keller <geekboy1011@gmail.com>
2021-07-05 00:02:17 -0400

    Added non working tx.

--- code.py
+++ code.py
@@ -29,23 +29,25 @@ def rx():               #10 Instructions
 
 
 @rp2.asm_pio(out_init=(rp2.PIO.OUT_HIGH,rp2.PIO.OUT_HIGH),set_init=(rp2.PIO.OUT_HIGH,rp2.PIO.OUT_HIGH),out_shiftdir=rp2.PIO.SHIFT_RIGHT)
-
 def tx(): #12
     wrap_target()
-    pull(block)  #block until we have data
-    irq(1)       #raise irq to block RX
-    out(x,1)     #Put LSB into x
-    jmp(!x,'txwhitefirst') #bit is 0 then jump
-    set(pins,1)            #pull white low
-    wait(0,pins,1)         #wait for red to low
-    jmp('bitendend')       #jump to shared final wait
-    label('txwhitefirst')
-    set(pins,2)            #pull red low
-    wait(0,pins,2)         #wait for white to low
-    label('bitendend')
-    set(pins,3)            #Free both line assertions   
-    wait(1,pins,0)         #
-    wait(1,pins,1)         #Wait for both lines to be deasserted
+    irq(0)                  #clear any pending RX irq
+    irq(clear,1)            #clear rx spinlock irq
+    pull(ifempty)           #block until we have data
+    irq(1)                  #raise irq to block RX
+    out(x,1)                #Put LSB into x
+    jmp(not_x,'tx_0_bit')      #bit is 0 then jump
+    set(pins,1)             #pull white low
+    wait(0,pins,1)          #wait for red to low
+    jmp('bit_final_stage')  #jump to shared final wait
+    label('tx_0_bit')
+    set(pins,2)             #pull red low
+    wait(0,pins,2)          #wait for white to low
+    label('bit_final_stage')
+    set(pins,3)             #Free both line assertions   
+    wait(1,pins,0)          #
+    wait(1,pins,1)          #Wait for both lines to be deasserted
+    mov(x,null)
     wrap()    
 
 #Pin watch routine
@@ -70,17 +72,26 @@ whiteOut = machine.Pin(3, machine.Pin.OUT)
 
 
 #In pin is 1, Because we care about the white line....
-mainStateMachine = rp2.StateMachine(0, rx,freq=20000, set_base=machine.Pin(2), in_base=machine.Pin(0),out_base=machine.Pin(2),push_thresh=8,jmp_pin=machine.Pin(0) )
-redWatchMachine = rp2.StateMachine(1, pinwatch, freq=20000,in_base=machine.Pin(0))
-whiteWatchMachine = rp2.StateMachine(2, pinwatch, freq=20000,in_base=machine.Pin(1))
+rxStateMachine = rp2.StateMachine(0, rx,freq=2000, set_base=machine.Pin(2), in_base=machine.Pin(0),out_base=machine.Pin(2),push_thresh=8,jmp_pin=machine.Pin(0) )
+txStateMachine = rp2.StateMachine(1, tx,freq=2000, set_base=machine.Pin(2), in_base=machine.Pin(0),out_base=machine.Pin(2),pull_thresh=8)
+redWatchMachine = rp2.StateMachine(2, pinwatch, freq=2000,in_base=machine.Pin(0))
+whiteWatchMachine = rp2.StateMachine(3, pinwatch, freq=2000,in_base=machine.Pin(1))
 print("Starting state machines...")
-mainStateMachine.active(1)
+rxStateMachine.active(1)
+txStateMachine.active(1)
 redWatchMachine.active(1)
 whiteWatchMachine.active(1)
 
 
+print('sending')
+txStateMachine.put(0)
+txStateMachine.put(0xFF)
+print("rxing")
 # Continually start and stop state machine
 while True:    
-    print(hex(sm.get()>>24))  ##Shift all bits >>24 because they are in MSB of teh input register
+    print(rxStateMachine.get()>>24)  ##Shift all bits >>24 because they are in MSB of teh input register
 
 
+RDY = [0xFF,0x23,0x68,0,0,0,0x8B,0]
+for b in RDY:
+    txStateMachine.put(b)
